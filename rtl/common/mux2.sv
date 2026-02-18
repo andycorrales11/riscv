f6e035a34b8ffeb6f94b374300bcc400a41fd1ba
andycorrales11 <andycorrales7@gmail.com>
2026-02-18 10:12:33 -0600

    simplified mux implementation

--- rtl/common/mux2.sv
+++ rtl/common/mux2.sv
@@ -6,10 +6,7 @@ module mux2 #(parameter WIDTH = 32) (
   input logic [WIDTH-1:0] in1,
   output logic [WIDTH-1:0] out
 );
+  
+  assign out = (sel == 1'b0) ? in0 : in1;
 
-  always_comb begin
-    case (sel)
-      1'b0: out = in0;
-      1'b1: out = in1;
-    endcase
-  end
+endmodule 
