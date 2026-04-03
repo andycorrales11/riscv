2c9597a0bf465a02f39ae74190688d7c772a96cf
andycorrales11 <andycorrales7@gmail.com>
2026-04-03 11:05:49 -0500

    Started Ref Model instruction checking

--- tb/uvm/rv32i_ref_model.sv
+++ tb/uvm/rv32i_ref_model.sv
@@ -27,11 +27,88 @@ class rv32i_ref_model extends uvm_object;
   function int step(output logic [4:0] rd, output logic [31:0] wr_data);
     logic [31:0] instr;
     instr = imem[pc >> 2];
-    // FINISH ME: Implement reference model execution logic here
 
+    case (instr[6:0])
+      7'b0110011: begin // R-type
+        logic [9:0] funct = {instr[31:25], instr[14:12]};
+        logic [4:0] rs1 = instr[19:15];
+        logic [4:0] rs2 = instr[24:20];
+        rd  = instr[11:7];
+        logic [31:0] num1 = rf[rs1];
+        logic [31:0] num2 = rf[rs2];
+        case (funct)
+          10'b0000000000: rf[rd] = num1 + num2; // ADD
+          10'b0100000000: rf[rd] = num1 - num2; // SUB
+          10'b0000000001: rf[rd] = num1 << num2[4:0]; // SLL
+          10'b0000000010: rf[rd] = ($signed(num1) < $signed(num2)) ? 32'h1 : 32'h0; // SLT
+          // 10'b0000000011: rf[rd] = SLTU - (NOT IMPLEMENTED)
+          10'b0000000100: rf[rd] = num1 ^ num2; // XOR
+          10'b0000000101: rf[rd] = num1 >> num2[4:0]; // SRL
+          10'b0100000101: rf[rd] = $signed(num1) >>> num2[4:0]; // SRA
+          10'b0000000110: rf[rd] = num1 | num2; // OR
+          10'b0000000111: rf[rd] = num1 & num2; // AND
+          default: begin
+            $display("Unknown R-type instruction at PC = %h", pc);
+          end
+        endcase
+
+        pc += 4;
+        wr_data = 0;
+        return 0;
+      end
+      7'b0010011: begin // I-type
+        logic [2:0] funct3 = instr[14:12];
+        logic [4:0] rs1 = instr[19:15];
+        rd  = instr[11:7];
+        logic [31:0] num1 = rf[rs1];
+        logic [31:0] imm = {{20{instr[31]}}, instr[31:20]};
+        case (funct3)
+          3'b000: rf[rd] = num1 + imm; // ADDI
+          3'b010: rf[rd] = ($signed(num1) < $signed(imm)) ? 32'h1 : 32'h0; // SLTI
+          3'b100: rf[rd] = num1 ^ imm; // XORI
+          3'b110: rf[rd] = num1 | imm; // ORI
+          3'b111: rf[rd] = num1 & imm; // ANDI
+          3'b001: rf[rd] = num1 << imm[4:0]; // SLLI
+          3'b101: begin
+            if (imm[11:5] == 7'b0000000) rf[rd] = num1 >> imm[4:0]; // SRLI
+            else if (imm[11:5] == 7'b0100000) rf[rd] = $signed(num1) >>> imm[4:0]; // SRAI
+            else begin
+              $display("Unknown I-type shift instruction at PC = %h", pc);     
+            end
+          end
+        endcase 
+        pc += 4;
+        wr_data = 0;
+        return 0;
+      end
+      7'b0000011: begin // I-Type Load
+        
+      end
+      7'b0100011: begin // S-Type
+        
+      end
+      7'b1100011: begin // B-Type
+        
+      end
+      7'b1101111: begin //JAL
+
+      end
+      7'b1100111: begin // JALR
+
+      end
+      7'b0110111: begin // LUI
+
+      end
+      7'b0010111: begin //AUIPC
+
+      end
+      default: begin
+        $display("Unknown instruction at PC = %h", pc);
+      end
+    endcase
     rd = 5'b0;
     wr_data = 32'b0;
-    return 0; // return 1 for writes, 0 otherwise
+    return 0; 
   endfunction
 
 endclass
--- tb/uvm/rv32i_scoreboard.sv
+++ tb/uvm/rv32i_scoreboard.sv
@@ -1,17 +1,39 @@
 class rv32i_scoreboard extends uvm_scoreboard;
+  `uvm_component_utils(rv32i_scoreboard)
 
-  rv32i_cpu_expected_state expected_state[$];
-  rv32i_cpu_actual_state   actual_state[$];
+  `uvm_analysis_imp_decl(_driver)
+  `uvm_analysis_imp_decl(_monitor)
 
-  uvm_analysis_imp #(rv32i_monitor_txn, rv32i_scoreboard) imp;
+  `uvm_analysis_imp_driver #(rv32i_seq_item,     rv32i_scoreboard) driver_imp;
+  `uvm_analysis_imp_monitor #(rv32i_monitor_txn, rv32i_scoreboard) monitor_imp;
+  rv32i_ref_model ref_model;
+
 
   function new (string name, uvm_component parent);
     super.new(name, parent);
     imp = new("imp", this);
   endfunction
 
-  function void write(input rv32i_monitor_txn txn);
-    // FINISH ME: Implement scoreboard checking logic here
+  function void write_driver(input rv32i_seq_item item);
+    ref_model.load_program(item);
+  endfunction
+
+  function void write_monitor(input rv32i_monitor_txn txn);
+    logic [4:0] predicted_rd;
+    logic [31:0] predicted_val;
+
+    if (txn.reset) return;
+
+    ref_model.step(predicted_rd, predicted_val);
+
+    if (txn.reg_write) begin
+      if (predicted_rd !== txn.rd || predicted_val !== txn.wr_data) begin
+        `uvm_error(get_type_name(), $sformatf("Mismatch at PC = %h: expected rd = %0d, wr_data = %h; got rd = %0d, wr_data = %h",
+          txn.pc, predicted_rd, predicted_val, txn.rd, txn.wr_data))
+      end else begin
+        `uvm_info(get_type_name(), $sformatf("Match at PC = %h: rd = %0d, wr_data = %h", txn.pc, txn.rd, txn.wr_data), UVM_LOW)
+      end
+    end
   endfunction
 
 endclass
